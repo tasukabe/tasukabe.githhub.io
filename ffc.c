851ac7f60f626f7d6b60a25227057ce357817d26
tasukabe <90055562+tasukabe@users.noreply.github.com>
2021-09-09 03:07:41 -0700

    Add files via upload

---
+++ ffc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+main()
+{ int a,b,sum;
+  printf("Enter the first number=") ;
+  scanf("%d",&a) ;
+  printf("Enter the second number=") ;
+  scanf("%d", &b) ;
+  sum=a+b;
+  printf("Sum of two number = %d\n", sum);
+   printf("") ;
+  return 0;
+}
